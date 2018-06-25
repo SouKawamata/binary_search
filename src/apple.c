fca98ac7833d8e21f08d98c13786f7776dc107f8
SouKawamata <kawamata.s.ac@m.titech.ac.jp>
2018-06-25 20:06:25 +0900

    work.c途中

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,37 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+    int i, a, rest;
+    rest = k;
+    for(i = 0; i < n; i++){
+        a = A[i];
+        while (a > 0) {
+            a -= m;
+            rest--;
+        }
+    }
+    return rest >= 0;
+}
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    
+    lb = 0;
+    ub = 1000000000;
+    while(ub - lb > 1){
+        int m = (lb + ub)/2;
+        if(p(m)){
+            ub = m;
+        }
+        else {
+            lb = m;
+        }
+    }
+    printf("%d\n",ub);
+    return 0;
 }
--- src/spear.c
+++ src/spear.c
@@ -4,14 +4,35 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+    int i, a, rest;
+    rest = k;
+    for(i = 0; i < n; i++){
+        a = A[i]/m;
+        rest -= a;
+    }
+    return rest <= 0;
+}
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    
+    lb = 0;
+    ub = 1000000000;
+    while(ub - lb > 1){
+        int m = (lb + ub)/2;
+        if(p(m)){
+            lb = m;
+        }
+        else {
+            ub = m;
+        }
+    }
+    printf("%d\n",lb);
+    return 0;
 }
+
